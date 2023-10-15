9aa2d81bdedb3a77fe8d33ef92eb2efde702143e
teplodizain <117904330+teplodizain@users.noreply.github.com>
2023-10-15 22:40:18 +0700

    Delete code/DZ_2 directory

--- code/DZ_2/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  VM1_name = "${var.vm_web_name1}"
-  VM2_name = "${var.vm_db_name}" 
-}
--- code/DZ_2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "one_VM" {
-  value = yandex_compute_instance.platform.network_interface.0.nat_ip_address
-
-}
-
-output "two_vm" {
-  value = yandex_compute_instance.platform_2.network_interface.0.nat_ip_address
-
-}
